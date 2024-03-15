5d369f58b65dc86cd2af98269a0b85a44dcbc5f7
Kacp <kacperkupczynski2017@gmail.com>
2024-03-15 14:59:26 +0100

    added buttons toggle

--- src/form/Education.jsx
+++ src/form/Education.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 import Button from "../Button.jsx";
+import {useState} from "react";
 
 const Education = ({ items, addItem, onChange, section, deleteItem }) => {
 
+    const [toggle, setToggle] = useState(true)
 
     return (
         <div className='section'>
@@ -10,33 +12,38 @@ const Education = ({ items, addItem, onChange, section, deleteItem }) => {
                 return (
                     <div className='item' key={item.id}>
                         <h2 className='title'>Education #{index + 1}</h2>
-                        <input
-                            type='text'
-                            placeholder='School'
-                            value={item.school}
-                            onChange={e => onChange(section, index, 'school', e.target.value)}
-                        />
-                        <input
-                            type='text'
-                            placeholder='Degree'
-                            value={item.degree}
-                            onChange={e => onChange(section, index, 'degree', e.target.value)}
-                        />
-                        <input
-                            type={'text'}
-                            placeholder={'Start Date'}
-                            value={item.startDate}
-                            onChange={e => onChange(section, index, 'startDate', e.target.value)}
-                        />
-                        <input
-                            type='text'
-                            placeholder='Graduation Date'
-                            value={item.graduationDate}
-                            onChange={e => onChange(section, index, 'graduationDate', e.target.value)}
-                        />
+                        <Button title='^' onClick={() => setToggle(!toggle)}></Button>
+                        {toggle ? (
+                            <>
+                                <input
+                                    type='text'
+                                    placeholder='School'
+                                    value={item.school}
+                                    onChange={e => onChange(section, index, 'school', e.target.value)}
+                                />
+                                <input
+                                    type='text'
+                                    placeholder='Degree'
+                                    value={item.degree}
+                                    onChange={e => onChange(section, index, 'degree', e.target.value)}
+                                />
+                                <input
+                                    type={'text'}
+                                    placeholder={'Start Date'}
+                                    value={item.startDate}
+                                    onChange={e => onChange(section, index, 'startDate', e.target.value)}
+                                />
+                                <input
+                                    type='text'
+                                    placeholder='Graduation Date'
+                                    value={item.graduationDate}
+                                    onChange={e => onChange(section, index, 'graduationDate', e.target.value)}
+                                />
+                            </>
+                        ) : null}
                         <div className='buttons'>
-                            <Button title='Add' onClick={() => addItem(section)} />
-                            <Button title='Remove' onClick={() => deleteItem(section, item.id)} />
+                            <Button title='Add' onClick={() => addItem(section)}/>
+                            <Button title='Remove' onClick={() => deleteItem(section, item.id)}/>
                         </div>
                     </div>
                 )
@@ -44,7 +51,7 @@ const Education = ({ items, addItem, onChange, section, deleteItem }) => {
                 <div className='item'>
                     <h2 className='title'>Education</h2>
                     <div className='buttons'>
-                        <Button title='Add' onClick={() => addItem(section)} />
+                        <Button title='Add' onClick={() => addItem(section)}/>
                     </div>
                 </div>
             )}
--- src/form/Experience.jsx
+++ src/form/Experience.jsx
@@ -1,49 +1,58 @@
 import PropTypes from 'prop-types';
 import Button from "../Button.jsx";
+import {useState} from "react";
 
 const Experience = ({ items, addItem, onChange, section, deleteItem }) => {
+    const [toggle, setToggle] = useState(false)
+
     return (
         <div className='section'>
             {items.length > 0 ? items.map((item, index) => {
                 return (
                     <div className='item' key={item.id}>
                         <h2 className='title'>Experience #{index + 1}</h2>
-                        <input
-                            type='text'
-                            placeholder='Company'
-                            value={item.company}
-                            onChange={e => onChange(section, index, 'company', e.target.value)}
-                        />
-                        <input
-                            type='text'
-                            placeholder='Position'
-                            value={item.position}
-                            onChange={e => onChange(section, index, 'position', e.target.value)}
-                        />
-                        <input
-                            type='text'
-                            placeholder='Main Tasks'
-                            value={item.mainTasks}
-                            onChange={e => onChange(section, index, 'mainTasks', e.target.value)}
-                        />
-                        <input
-                            type='text'
-                            placeholder='Start Date'
-                            value={item.startDate}
-                            onChange={e => onChange(section, index, 'startDate', e.target.value)}
-                        />
-                        <input
-                            type='text'
-                            placeholder='End Date'
-                            value={item.endDate}
-                            onChange={e => onChange(section, index, 'endDate', e.target.value)}
-                        />
-                        <div className='buttons'>
-                            <Button title='Add' onClick={() => addItem(section)} />
-                            <Button title='Remove' onClick={() => deleteItem(section, item.id)} />
-                        </div>
+                        <Button title='^' onClick={() => setToggle(!toggle)}></Button>
+                        {toggle ? (
+                            <>
+                                <input
+                                    type='text'
+                                    placeholder='Company'
+                                    value={item.company}
+                                    onChange={e => onChange(section, index, 'company', e.target.value)}
+                                />
+                                <input
+                                    type='text'
+                                    placeholder='Position'
+                                    value={item.position}
+                                    onChange={e => onChange(section, index, 'position', e.target.value)}
+                                />
+                                <input
+                                    type='text'
+                                    placeholder='Main Tasks'
+                                    value={item.mainTasks}
+                                    onChange={e => onChange(section, index, 'mainTasks', e.target.value)}
+                                />
+                                <input
+                                    type='text'
+                                    placeholder='Start Date'
+                                    value={item.startDate}
+                                    onChange={e => onChange(section, index, 'startDate', e.target.value)}
+                                />
+                                <input
+                                    type='text'
+                                    placeholder='End Date'
+                                    value={item.endDate}
+                                    onChange={e => onChange(section, index, 'endDate', e.target.value)}
+                                />
+                                <div className='buttons'>
+                                    <Button title='Add' onClick={() => addItem(section)} />
+                                    <Button title='Remove' onClick={() => deleteItem(section, item.id)} />
+                                </div>
+                            </>
+                        ) : null}
                     </div>
                 )
+
             }) : (
                 <div className='item'>
                     <h2 className='title'>Experience</h2>
